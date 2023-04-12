d9fd12703f6b2b6510e0331d540b998d0471c790
DarioCepele <dario.cepele3@gmail.com>
2023-04-12 16:49:00 +0200

    some edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      current: 'currentColor',
+      'white': '#ffffff',
+      'yellow': '#FF9F1C',
+      'blue': '#011627',
+      'red': '#ff0000'
+    }
   },
   plugins: [],
 }
