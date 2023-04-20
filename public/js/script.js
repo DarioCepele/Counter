17cbad7f47f29eafa9551a37a9518156c569594c
DarioCepele <dario.cepele3@gmail.com>
2023-04-20 12:36:19 +0200

    Fix all

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,29 @@
-let counter = document.querySelector(".value");
+// Elements initialization
+const div = document.querySelector(".div");
+let counter = document.createElement("p");
+let space = document.createElement("br");
+let incrementButton = document.createElement("button")
+let decreaseButton = document.createElement("button")
 
-// Function to set the value
+// Assigning values
+counter.innerText = 0;
+incrementButton.innerHTML = "+";
+decreaseButton.innerHTML = "-";
+
+// Function to append items and load style
 document.addEventListener("DOMContentLoaded", function () {
-    counter.innerHTML = 0;
+    div.appendChild(counter);
+    div.appendChild(incrementButton);
+    div.appendChild(space)
+    div.appendChild(decreaseButton);
+    counter.classList.add("font-semibold","text-5xl", "md:text-8xl", "lg:text-8xl", "font-grotesk");
+    incrementButton.classList.add("text-5xl", "rounded-lg", "mt-12", "mb-1", "h-16", "w-80", "border-2", "border-blue", "bg-blue", "font-grotesk");
+    decreaseButton.classList.add("text-5xl", "rounded-lg", "mt-2", "h-16", "w-80", "border-2", "border-blue", "bg-blue", "font-grotesk")
 })
+
+// EventListener for click
+incrementButton.addEventListener("click", increment)
+decreaseButton.addEventListener("click", decrement)
 
 // Function to increase the value
 function increment(){
